71d096d3cefd2108d51c088553e1d443a27edb00
Emmanuelsteph7 <64918381+Emmanuelsteph7@users.noreply.github.com>
2021-06-22 10:40:00 +0100

    Delete client directory

--- client/src/components/modal/Modal.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./modal.scss";
-
-const Modal = ({ modalBody, modalState }) => {
-  return <div className={`modal ${modalState && "show"}`}>{modalBody}</div>;
-};
-
-export default Modal;
